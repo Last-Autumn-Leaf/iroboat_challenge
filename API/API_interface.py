c6724f108040d96a413cb3d74570ed8c7729e7e6
TheShyDev-Yoogo <lina.ouaret@insa-lyon.fr>
2022-05-04 15:28:33 -0400

    Initial commit

---
+++ API/API_interface.py
@@ -0,0 +1,145 @@
+import requests
+import json
+import argparse
+
+class Roboat_API:
+    #class variable
+    INVALID_TOKEN = 'No Token'
+    INVALID_RESP=dict()
+    roboats_url = 'https://roboats.virtualregatta.com/api'
+    mail = "deepazurteam@outlook.com"
+    password = "Hidden"
+
+    def __init__(self,raceId=531,legNum=1,debug=False):
+        self.INVALID_TOKEN = 'No Token'
+        self.roboats_url = 'https://roboats.virtualregatta.com/api'
+        self.mail = "deepazurteam@outlook.com"
+        self.password = "4CAJHW"
+
+        self.token = self.INVALID_TOKEN
+        self.raceId = raceId
+        self.legNum = legNum
+        self.debug=debug
+
+        #------- Try to log ------
+        self.login()
+
+    def checkStatus(self,response, name=''):
+        if self.debug:
+            if response.status_code == 200:
+                print('Request ' + name + ' success')
+
+            else:
+                print('Request ' + name + ' failed')
+                if response.status_code == 404:
+                    print('\t' + response.text)
+                else:
+                    print('\t' + response.json()['error']['message'])
+                    print('\t' + response.json()['error']['type'], ' | ', response.json()['error']['code'])
+        try:
+            return response.json()
+        except ValueError:
+            return response.headers
+
+    def login(self):
+
+        bodyLogin = json.dumps({
+            "email": self.mail,
+            "password": self.password,
+            "raceId": self.raceId,
+            "legNum": self.legNum
+        })
+        headersLogin = {
+            'Content-Type': 'application/json'
+        }
+        logingUrl = "%s/login" % self.roboats_url
+        response = requests.request("POST", logingUrl, headers=headersLogin, data=bodyLogin)
+        if response.status_code == 200:
+            self.token = response.headers['Token']
+        else:
+            self.token = self.INVALID_TOKEN
+        return self.checkStatus(response, name="login")
+
+    def InfosFast(self):
+        if self.token == self.INVALID_TOKEN :
+            print('Invalid Token, trying to reconnect')
+            self.login()
+            if self.token == self.INVALID_TOKEN :
+                print("reconnection failed...")
+                return self.INVALID_RESP
+            else :
+                self.InfosFast()
+        else:
+            infoFastUrl = "%s/infos/fast" % self.roboats_url
+            payload = ""
+            headers = {
+                'Token': self.token
+            }
+            response = requests.request("GET", infoFastUrl, headers=headers, data=payload)
+            return self.checkStatus(response, name="infoFast")
+
+    def InfosSlow(self):
+        if self.token == self.INVALID_TOKEN :
+            print('Invalid Token, trying to reconnect')
+            self.login()
+            if self.token == self.INVALID_TOKEN :
+                print("reconnection failed...")
+                return self.INVALID_RESP
+            else :
+                self.InfosSlow()
+        else :
+            infoFastUrl = "%s/infos/slow" % self.roboats_url
+            payload = ""
+            headers = {
+                'Token': self.token
+            }
+            response = requests.request("GET", infoFastUrl, headers=headers, data=payload)
+            return self.checkStatus(response,name="infoSlow")
+
+    def BoatActions(self,tsLegStart: int, actions: list):
+        """
+        :param tsLegStart: Timestamp of the actions
+        :param actions: list of a dict of the actions like :
+            actions =[
+                    {
+                        "type": "heading",
+                        "values": {
+                            "deg": 200,
+                            "autoTwa": True
+                        }
+                    }
+                ]
+        :return: response (dict)
+        """
+        if self.token == self.INVALID_TOKEN :
+            print('Invalid Token, trying to reconnect')
+            self.login()
+            if self.token == self.INVALID_TOKEN :
+                print("reconnection failed...")
+                return self.INVALID_RESP
+            else :
+                self.BoatActions()
+        else :
+            boatActionstUrl = "%s/boat/actions" % self.roboats_url
+            payload = json.dumps({
+                "tsLegStart": tsLegStart,
+                "actions": actions
+            })
+            headers = {
+                'Token': self.token,
+                'Content-Type': 'application/json'
+            }
+
+            response = requests.request("PUT", boatActionstUrl, headers=headers, data=payload)
+            return self.checkStatus(response, name="Boat Action")
+
+if __name__ == '__main__':
+    #password should not be writtin in clear
+    #this is an attempt to hide it
+
+    parser = argparse.ArgumentParser("API_interface")
+    parser.add_argument("password", help="Password for connection", type=str)
+    args = parser.parse_args()
+    Roboat_API.password=args.password
+    api_caller= Roboat_API()
+    print(api_caller.token)
---
+++ API/API_test.py
@@ -0,0 +1,45 @@
+import unittest
+from API.API_interface import Roboat_API
+import argparse
+class Roboat_API_Test_failed(unittest.TestCase):
+    def setUp(self) -> None:
+        self.api_caller=Roboat_API(1, 1, 1)
+    def test_failed_login(self):
+        self.assertEqual(self.api_caller.token, self.api_caller.INVALID_TOKEN)  # add assertion here
+    def test_failed_infoFast(self):
+        self.assertEqual(self.api_caller.InfosFast(), self.api_caller.INVALID_RESP)
+    def test_failed_infoSlow(self):
+        self.assertEqual(self.api_caller.InfosSlow(), self.api_caller.INVALID_RESP)
+    def test_failed_boatAction(self):
+        TS = 1651686030000
+        action = [{
+            "type": "heading",
+            "values": {
+                "deg": 200,
+                "autoTwa": True}}]
+        self.assertEqual(self.api_caller.BoatActions(TS, action), self.api_caller.INVALID_RESP)
+
+class Roboat_API_Test_success(unittest.TestCase):
+    def setUp(self) -> None:
+        self.api_caller=Roboat_API(531, 1, 1)
+    def test_succeed_login(self):
+        self.assertNotEqual(self.api_caller.token, self.api_caller.INVALID_TOKEN)
+    def test_succeed_InfosFast(self):
+        self.assertEqual(self.api_caller.InfosFast()['rc'],'ok')
+    def test_succeed_InfosSlow(self):
+        self.assertEqual(self.api_caller.InfosSlow()['rc'],'ok')
+    def test_succeed_BoatAction(self):
+        TS = 1651686030000
+        action = [{
+            "type": "heading",
+            "values": {
+                "deg": 200,
+                "autoTwa": True}}]
+        self.assertEqual(self.api_caller.BoatActions(TS, action)['rc'],'ok')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("unittest for API_interface")
+    parser.add_argument("password", help="Password for connection", type=str)
+    args = parser.parse_args()
+    Roboat_API.password=args.password
+    unittest.main()
